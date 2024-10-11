182d203bc5b093ccdbe9881ff8d520bd17e4adca
Huy Le <ing.huyle@gmail.com>
2024-10-11 16:20:54 +0200

    refactor

--- src/DrumPad.jsx
+++ src/DrumPad.jsx
@@ -2,11 +2,9 @@ import './styles/DrumPad.scss';
 
 const DrumPad = ({ idDrum, handleClick, idAudio, src }) => {
   return (
-    <div className='drum-pad gray' id={idDrum} onClick={handleClick}>{idAudio}
-      <audio
-        id={idAudio}
-        src={src}>
-      </audio>
+    <div className='drum-pad gray' id={idDrum} onClick={handleClick}>
+      {idAudio}
+      <audio id={idAudio} src={src}></audio>
     </div>
   );
 }

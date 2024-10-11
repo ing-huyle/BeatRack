6aa8c96fcf16ac05548993f0067eb2569eed4c01
Huy Le <ing.huyle@gmail.com>
2024-10-11 18:15:31 +0200

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -76,15 +76,60 @@ const App = () => {
       <h1>Drum Machine</h1>
       <div id='display' className="gray">{text}</div>
       <div className='drum-pads'>
-        <DrumPad idDrum='Heater-1' handleClick={handleClick} idAudio={keys[0]} src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' />
-        <DrumPad idDrum='Heater-2' handleClick={handleClick} idAudio={keys[1]} src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3' />
-        <DrumPad idDrum='Heater-3' handleClick={handleClick} idAudio={keys[2]} src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3' />
-        <DrumPad idDrum='Heater-4' handleClick={handleClick} idAudio={keys[3]} src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3' />
-        <DrumPad idDrum='Clap' handleClick={handleClick} idAudio={keys[4]} src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3' />
-        <DrumPad idDrum='Open-HH' handleClick={handleClick} idAudio={keys[5]} src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3' />
-        <DrumPad idDrum='Kick-n-Hat' handleClick={handleClick} idAudio={keys[6]} src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3' />
-        <DrumPad idDrum='Kick' handleClick={handleClick} idAudio={keys[7]} src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3' />
-        <DrumPad idDrum='Closed-HH' handleClick={handleClick} idAudio={keys[8]} src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3' />
+        <DrumPad
+          idDrum='Heater-1'
+          handleClick={handleClick}
+          idAudio={keys[0]}
+          src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+        />
+        <DrumPad
+          idDrum='Heater-2'
+          handleClick={handleClick}
+          idAudio={keys[1]}
+          src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3'
+        />
+        <DrumPad
+          idDrum='Heater-3'
+          handleClick={handleClick}
+          idAudio={keys[2]}
+          src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3'
+        />
+        <DrumPad
+          idDrum='Heater-4'
+          handleClick={handleClick}
+          idAudio={keys[3]}
+          src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3'
+        />
+        <DrumPad
+          idDrum='Clap'
+          handleClick={handleClick}
+          idAudio={keys[4]}
+          src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3'
+        />
+        <DrumPad
+          idDrum='Open-HH'
+          handleClick={handleClick}
+          idAudio={keys[5]}
+          src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3'
+        />
+        <DrumPad
+          idDrum='Kick-n-Hat'
+          handleClick={handleClick}
+          idAudio={keys[6]}
+          src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3'
+        />
+        <DrumPad
+          idDrum='Kick'
+          handleClick={handleClick}
+          idAudio={keys[7]}
+          src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3'
+        />
+        <DrumPad
+          idDrum='Closed-HH'
+          handleClick={handleClick}
+          idAudio={keys[8]}
+          src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3'
+        />
       </div>
     </div>
   )
